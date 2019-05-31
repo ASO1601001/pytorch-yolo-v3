ccd1676ea6fe7f14668eace7c6347b689abd6718
shun <tidamonogatari@gmail.com>
2019-05-31 09:49:06 +0900

    スマホ見つけたら座標と画像出力（カメラ側完成）

--- cam_demo.py
+++ cam_demo.py
@@ -12,6 +12,7 @@ import pandas as pd
 import random 
 import argparse
 import pickle as pkl
+import sys
 
 def get_test_input(input_dim, CUDA):
     img = cv2.imread("imgs/messi.jpg")
@@ -46,14 +47,20 @@ def write(x, img):
     c1 = tuple(x[1:3].int())
     c2 = tuple(x[3:5].int())
     cls = int(x[-1])
-    label = "{0}".format(classes[cls])
-    color = random.choice(colors)
-    cv2.rectangle(img, c1, c2,color, 1)
-    t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
-    c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
-    cv2.rectangle(img, c1, c2,color, -1)
-    cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1);
-    return img
+    c3 = str(x[1:3].int()) + "\n" + str(x[3:5].int())   # 変数c3にc1とc2の座標を代入
+    label = "{0}".format(classes[cls])  # Label Name ex)cell phone
+    if label.find("cell phone") != -1 : # cell phoneの場合 
+        print('ラベル名：')
+        sys.stdout.write(label)
+        print('座標：')
+        sys.stdout.write(c3)
+        img = np.asarray(img)
+        # cv2.imwrite('data/temp/img/' + 'test.jpg', img)   # 格納場所にwebカメラの静止画を保存
+        cv2.imwrite('outputs/test.jpg', img)
+        outputFile = 'outputs/tensor.txt' # 座標データ格納場所
+        # テキストファイル（tensor.txt）にc3を書き込み
+        with open(outputFile,'w',newline="\n") as fo:
+            fo.write(c3)
 
 def arg_parse():
     """
